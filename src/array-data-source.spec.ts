9dad17b40008c61cc048e9db53bb8283599d5d85
Patrick Stanford <contact@pathurs.com>
2018-07-10 22:09:56 +1000

    Add basic filtering and sorting to ArrayDataSource

--- src/array-data-source.spec.ts
+++ src/array-data-source.spec.ts
@@ -1,4 +1,57 @@
-import { TestFixture } from 'alsatian';
+import { Expect, IgnoreTest, Test, TestCase, TestFixture } from 'alsatian';
+import { ArrayDataSource } from './array-data-source';
 
 @TestFixture('ArrayDataSource')
-export class ArrayDataSourceTests {}
+export class ArrayDataSourceTests {
+    @TestCase([''])
+    @TestCase([0])
+    @TestCase([true])
+    @TestCase([null])
+    @TestCase([undefined])
+    @TestCase([() => null])
+    @TestCase([{}])
+    @TestCase([[]])
+    @Test('should be created')
+    public construct1<T>(data: T[]) {
+        const dataSource = new ArrayDataSource(data);
+
+        Expect(dataSource).toBeDefined();
+        Expect(dataSource instanceof ArrayDataSource).toBe(true);
+    }
+
+    @IgnoreTest('TODO')
+    @Test('filter() should add a filter to the processors')
+    public filter1<T>(data: T[]) {
+        /**/
+    }
+
+    @IgnoreTest('TODO')
+    @Test('filterBy() should add a filter to the processors')
+    public filterBy1<T>(data: T[]) {
+        /**/
+    }
+
+    @IgnoreTest('TODO')
+    @Test('removeFilter() should remove a filter from the processors')
+    public removeFilter1<T>(data: T[]) {
+        /**/
+    }
+
+    @IgnoreTest('TODO')
+    @Test('sort() should add a sorter to the processors')
+    public sort1<T>(data: T[]) {
+        /**/
+    }
+
+    @IgnoreTest('TODO')
+    @Test('sortBy() should add a sorter to the processors')
+    public sortBy1<T>(data: T[]) {
+        /**/
+    }
+
+    @IgnoreTest('TODO')
+    @Test('removeSort() should remove a sorter from the processors')
+    public removeSort1<T>(data: T[]) {
+        /**/
+    }
+}
--- src/array-data-source.ts
+++ src/array-data-source.ts
@@ -1,3 +1,60 @@
-import { DataSource } from './data-source';
+import { DataSource, DataSourceProcessor } from './data-source';
 
-export class ArrayDataSource<TArrayData> extends DataSource<TArrayData[]> {}
+export type ArrayDataSourceFilter<TData> = (value: TData, index: number, array: TData[]) => boolean;
+export type ArrayDataSourceSorter<TData> = (data1: TData, data2: TData) => 1 | 0 | -1;
+
+export class ArrayDataSource<TData> extends DataSource<TData[]> {
+    protected filterProcessor?: DataSourceProcessor<TData[]>;
+    protected sortProcessor?: DataSourceProcessor<TData[]>;
+
+    public filter(filter: ArrayDataSourceFilter<TData>): TData[] {
+        this.removeFilter();
+
+        this.filterProcessor = (data: TData[]) => data.filter(filter);
+
+        return this.addProcessor(this.filterProcessor);
+    }
+
+    public filterBy<TKey extends keyof TData, TValue extends TData[TKey]>(property: TKey, value: TValue): TData[] {
+        return this.filter(data => data[property] === value);
+    }
+
+    public removeFilter() {
+        return this.filterProcessor ? this.removeProcessor(this.filterProcessor) : this.processedData;
+    }
+
+    public sort(sorter: ArrayDataSourceSorter<TData>): TData[] {
+        if (this.sortProcessor) {
+            this.removeProcessor(this.sortProcessor);
+        }
+
+        this.sortProcessor = (data: TData[]) => data.sort(sorter);
+
+        return this.addProcessor(this.sortProcessor);
+    }
+
+    public sortBy<TKey extends keyof TData>(property: TKey): TData[] {
+        return this.sort((data1, data2) => {
+            if (!data1 && !data2) {
+                return 0;
+            } else if (!data1) {
+                return -1;
+            } else if (!data2) {
+                return 1;
+            } else {
+                const data1String = `${data1[property]}`;
+                const data2String = `${data2[property]}`;
+
+                if (data1String === data2String) {
+                    return 0;
+                } else {
+                    return data1String > data2String ? 1 : -1;
+                }
+            }
+        });
+    }
+
+    public removeSort() {
+        return this.sortProcessor ? this.removeProcessor(this.sortProcessor) : this.processedData;
+    }
+}
