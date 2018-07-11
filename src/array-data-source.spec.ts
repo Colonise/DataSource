5d03629ce94072d25474a78aed0b91b81fa496ea
Patrick Stanford <contact@pathurs.com>
2018-07-11 22:07:52 +1000

    Add Unit Tests for filtering and sorting, and fix some revealed bugs

--- src/array-data-source.spec.ts
+++ src/array-data-source.spec.ts
@@ -1,5 +1,5 @@
-import { Expect, IgnoreTest, Test, TestCase, TestFixture } from 'alsatian';
-import { ArrayDataSource } from './array-data-source';
+import { Any, Expect, IgnoreTest, SpyOn, Test, TestCase, TestFixture } from 'alsatian';
+import { ArrayDataSource, ArrayDataSourceFilter, ArrayDataSourceSorter } from './array-data-source';
 
 @TestFixture('ArrayDataSource')
 export class ArrayDataSourceTests {
@@ -19,39 +19,121 @@ export class ArrayDataSourceTests {
         Expect(dataSource instanceof ArrayDataSource).toBe(true);
     }
 
-    @IgnoreTest('TODO')
+    @TestCase([{ a: 1 }, { a: 0 }, { a: 1 }, { a: 0 }, { a: 1 }], [{ a: 0 }, { a: 0 }], (data: { a: 1 | 0 }) => !data.a)
+    @TestCase([1, 2, 3, 4, 5], [1, 3, 5], (data: number) => data % 2 !== 0)
+    @TestCase([0, '', undefined, null, false], [], (data: any) => !!data)
     @Test('filter() should add a filter to the processors')
-    public filter1<T>(data: T[]) {
-        /**/
+    public filter1<T>(data: T[], expected: T[], filter: ArrayDataSourceFilter<T>) {
+        const dataSource = new ArrayDataSource(data);
+        const spyable = { filter };
+        const filterSpy = SpyOn(spyable, 'filter');
+
+        const actual = dataSource.filter(spyable.filter);
+
+        Expect(filterSpy)
+            .toHaveBeenCalledWith(Any, Any(Number), Any(Array))
+            .exactly(data.length);
+        Expect(actual).toEqual(expected);
     }
 
-    @IgnoreTest('TODO')
+    @TestCase([0, 1, 2, 3, 4, 5], [1, 2, 3, 4, 5])
+    @TestCase([0, '', undefined, null, false], [])
     @Test('filterBy() should add a filter to the processors')
-    public filterBy1<T>(data: T[]) {
-        /**/
+    public filterBy1<T>(data: T[], expected: T[]) {
+        const dataSource = new ArrayDataSource(data);
+
+        const actual = dataSource.filterBy();
+
+        Expect(actual).toEqual(expected);
     }
 
-    @IgnoreTest('TODO')
+    @TestCase([[], [1], [2], [3], []], [[1], [2], [3]], 'length')
+    @TestCase([{ a: 1 }, { a: 0 }, { a: 1 }, { a: 0 }, { a: 1 }], [{ a: 1 }, { a: 1 }, { a: 1 }], 'a')
+    @Test('filterBy(property) should add a filter to the processors')
+    public filterBy2<T>(data: T[], expected: T[], property: keyof T) {
+        const dataSource = new ArrayDataSource(data);
+
+        const actual = dataSource.filterBy(property);
+
+        Expect(actual).toEqual(expected);
+    }
+
+    @TestCase([[], [1], [2], [3], []], [[], []], 'length', 0)
+    @TestCase([{ a: 1 }, { a: 0 }, { a: 1 }, { a: 0 }, { a: 1 }], [{ a: 0 }, { a: 0 }], 'a', 0)
+    @Test('filterBy(property, value) should add a filter to the processors')
+    public filterBy3<T>(data: T[], expected: T[], property: keyof T, value: T[keyof T]) {
+        const dataSource = new ArrayDataSource(data);
+
+        const actual = dataSource.filterBy(property, value);
+
+        Expect(actual).toEqual(expected);
+    }
+
+    @TestCase([0, '', undefined, null, false], [])
+    @TestCase([0, 1, 2, 3, 4, 5], [1, 2, 3, 4, 5])
     @Test('removeFilter() should remove a filter from the processors')
-    public removeFilter1<T>(data: T[]) {
-        /**/
+    public removeFilter1<T>(data: T[], expectedFiltered: T[]) {
+        const dataSource = new ArrayDataSource(data);
+
+        const actual1 = dataSource.filter(dataItem => !!dataItem);
+        const actual2 = dataSource.removeFilter();
+
+        Expect(actual1).toEqual(expectedFiltered);
+        Expect(actual2).toEqual(data);
     }
 
-    @IgnoreTest('TODO')
+    @TestCase(
+        ['a', 'e', 'b', 'd', 'c'],
+        ['a', 'b', 'c', 'd', 'e'],
+        (a: string, b: string) => (a === b ? 0 : a > b ? 1 : -1)
+    )
+    @TestCase([5, 4, 3, 2, 1], [1, 2, 3, 4, 5], (a: number, b: number) => (a === b ? 0 : a > b ? 1 : -1))
     @Test('sort() should add a sorter to the processors')
-    public sort1<T>(data: T[]) {
-        /**/
+    public sort1<T>(data: T[], expected: T[], sorter: ArrayDataSourceSorter<T>) {
+        const dataSource = new ArrayDataSource(data);
+        const spyable = { sorter };
+        const sorterSpy = SpyOn(spyable, 'sorter');
+
+        const actual = dataSource.sort(spyable.sorter);
+
+        Expect(sorterSpy)
+            .toHaveBeenCalledWith(Any, Any)
+            .greaterThan(data.length);
+        Expect(actual).toEqual(expected);
     }
 
-    @IgnoreTest('TODO')
+    @TestCase(['a', 'e', 'b', 'd', 'c'], ['a', 'b', 'c', 'd', 'e'])
+    @TestCase([5, 4, 3, 2, 1], [1, 2, 3, 4, 5])
     @Test('sortBy() should add a sorter to the processors')
-    public sortBy1<T>(data: T[]) {
-        /**/
+    public sortBy2<T>(data: T[], expected: T[]) {
+        const dataSource = new ArrayDataSource(data);
+
+        const actual = dataSource.sortBy();
+
+        Expect(actual).toEqual(expected);
     }
 
-    @IgnoreTest('TODO')
+    @TestCase([{ a: 1 }, { a: 0 }, { a: 1 }, { a: 0 }, { a: 1 }], [{ a: 1 }, { a: 0 }, { a: 1 }, { a: 0 }, { a: 1 }])
+    @TestCase([{ a: 1 }, { a: 0 }, { a: 1 }, { a: 0 }, { a: 1 }], [{ a: 1 }, { a: 0 }, { a: 1 }, { a: 0 }, { a: 1 }])
+    @Test('sortBy(property) should add a sorter to the processors')
+    public sortBy1<T>(data: T[], expected: T[], property: keyof T) {
+        const dataSource = new ArrayDataSource(data);
+
+        const actual = dataSource.sortBy(property);
+
+        Expect(actual).toEqual(expected);
+    }
+
+    @TestCase(['a', 'e', 'b', 'd', 'c'], ['a', 'b', 'c', 'd', 'e'])
+    @TestCase([5, 4, 3, 2, 1], [1, 2, 3, 4, 5])
     @Test('removeSort() should remove a sorter from the processors')
-    public removeSort1<T>(data: T[]) {
-        /**/
+    public removeSort1<T>(data: T[], expectedSorted: T[]) {
+        const dataSource = new ArrayDataSource(data);
+
+        const actual1 = dataSource.sort((a, b) => (a === b ? 0 : a > b ? 1 : -1));
+        const actual2 = dataSource.removeSort();
+
+        Expect(actual1).toEqual(expectedSorted);
+        Expect(actual2).toEqual(data);
     }
 }
--- src/array-data-source.ts
+++ src/array-data-source.ts
@@ -7,6 +7,11 @@ export class ArrayDataSource<TData> extends DataSource<TData[]> {
     protected filterProcessor?: DataSourceProcessor<TData[]>;
     protected sortProcessor?: DataSourceProcessor<TData[]>;
 
+    /**
+     * Filters the data by passing each entry to the supplied filter
+     *
+     * @param filter The function to filter the data by
+     */
     public filter(filter: ArrayDataSourceFilter<TData>): TData[] {
         this.removeFilter();
 
@@ -15,14 +20,52 @@ export class ArrayDataSource<TData> extends DataSource<TData[]> {
         return this.addProcessor(this.filterProcessor);
     }
 
-    public filterBy<TKey extends keyof TData, TValue extends TData[TKey]>(property: TKey, value: TValue): TData[] {
-        return this.filter(data => data[property] === value);
+    /**
+     * Filters the data by whether the entry is truthy
+     *
+     * Essentially:
+     * (entry) => !!entry
+     */
+    public filterBy(): TData[];
+    /**
+     * Filters the data by whether the supplied property of each entry is truthy
+     *
+     * Essentially:
+     * (entry) => !!entry[property]
+     *
+     * @param property The property to filter the data by
+     */
+    public filterBy<TKey extends keyof TData>(property: TKey): TData[];
+    /**
+     * Filters the data by strict equality of the supplied property of each entry to the supplied value
+     *
+     * Essentially:
+     * (entry) => entry[property] === value
+     *
+     * @param property The property to filter the data by
+     * @param value The value to filter the data by
+     */
+    public filterBy<TKey extends keyof TData, TValue extends TData[TKey]>(property: TKey, value: TValue): TData[];
+    public filterBy<TKey extends keyof TData, TValue extends TData[TKey]>(property?: TKey, value?: TValue): TData[] {
+        return property == null
+            ? this.filter(entry => !!entry)
+            : arguments.length === 1
+                ? this.filter(entry => !!entry[property])
+                : this.filter(entry => entry[property] === value);
     }
 
+    /**
+     * Removes the data filter if it exists
+     */
     public removeFilter() {
         return this.filterProcessor ? this.removeProcessor(this.filterProcessor) : this.processedData;
     }
 
+    /**
+     * Sorts the data using the supplied sorter to compare each entry
+     *
+     * @param sorter The function to sort the data by
+     */
     public sort(sorter: ArrayDataSourceSorter<TData>): TData[] {
         if (this.sortProcessor) {
             this.removeProcessor(this.sortProcessor);
@@ -33,27 +76,53 @@ export class ArrayDataSource<TData> extends DataSource<TData[]> {
         return this.addProcessor(this.sortProcessor);
     }
 
-    public sortBy<TKey extends keyof TData>(property: TKey): TData[] {
-        return this.sort((data1, data2) => {
-            if (!data1 && !data2) {
-                return 0;
-            } else if (!data1) {
-                return -1;
-            } else if (!data2) {
-                return 1;
-            } else {
-                const data1String = `${data1[property]}`;
-                const data2String = `${data2[property]}`;
-
-                if (data1String === data2String) {
-                    return 0;
-                } else {
-                    return data1String > data2String ? 1 : -1;
-                }
-            }
-        });
+    /**
+     * Sorts the data by comparing each entry
+     *
+     * Essentially:
+     * (a, b) => a === b ? 0 : a > b ? 1 : -1;
+     */
+    public sortBy(): TData[];
+    /**
+     * Sorts the data by comparing the property of each entry
+     *
+     * Essentially:
+     * (a, b) => a[property] === b[property] ? 0 : a[property] > b[property] ? 1 : -1;
+     *
+     * @param property The property to sort the data by
+     */
+    public sortBy<TKey extends keyof TData>(property: TKey): TData[];
+    public sortBy<TKey extends keyof TData>(property?: TKey): TData[] {
+        return property == null
+            ? this.sort((entry1, entry2) => {
+                  if (entry1 === entry2) {
+                      return 0;
+                  } else if (entry1 > entry2) {
+                      return 1;
+                  } else {
+                      return -1;
+                  }
+              })
+            : this.sort((entry1, entry2) => {
+                  if (entry1 == null && entry2 == null) {
+                      return 0;
+                  } else if (entry1 == null) {
+                      return -1;
+                  } else if (entry2 == null) {
+                      return 1;
+                  } else if (entry1[property] === entry2[property]) {
+                      return 0;
+                  } else if (entry1[property] > entry2[property]) {
+                      return 1;
+                  } else {
+                      return -1;
+                  }
+              });
     }
 
+    /**
+     * Removes the data sort if it exists
+     */
     public removeSort() {
         return this.sortProcessor ? this.removeProcessor(this.sortProcessor) : this.processedData;
     }
