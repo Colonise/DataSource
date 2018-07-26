b056ef3302a61a5b00fcc565e644e20fa7104d97
Patrick Stanford <contact@pathurs.com>
2018-07-26 14:07:08 +1000

    Fix a bug where the ArrayProcessorApi did not expose the value's length

--- src/processors/array.ts
+++ src/processors/array.ts
@@ -3,27 +3,35 @@ import { ComplexProcessor, ComplexProcessorApi } from './complex';
 /**
  * The public API of an ArrayProcessor.
  */
-export interface ArrayProcessorApi<TEntry> extends ComplexProcessorApi<TEntry[]> {}
+export interface ArrayProcessorApi<TEntry>
+  extends ComplexProcessorApi<TEntry[]> {
+  /**
+   * The number of entries after processing.
+   */
+  readonly length: number;
+}
 
 /**
  * A processor that can be activated and deactived.
  *
  * Designed for complex proessing of arrays.
  */
-export abstract class ArrayProcessor<TEntry> extends ComplexProcessor<TEntry[]> {
-    /**
-     * The number of entries after processing.
-     */
-    public get length(): number {
-        return this.value.length;
-    }
+export abstract class ArrayProcessor<TEntry> extends ComplexProcessor<
+  TEntry[]
+> {
+  /**
+   * The number of entries after processing.
+   */
+  public get length(): number {
+    return this.value.length;
+  }
 
-    /**
-     * Creates a new ArrayProcessor.
-     *
-     * @param active Whether the ArrayProcessor should start active.
-     */
-    public constructor(active: boolean = true) {
-        super([], active);
-    }
+  /**
+   * Creates a new ArrayProcessor.
+   *
+   * @param active Whether the ArrayProcessor should start active.
+   */
+  public constructor(active: boolean = true) {
+    super([], active);
+  }
 }
